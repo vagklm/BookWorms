248e9244836d2d51f4ee7123fe7f90fbeb06d097
Vag_kal96 <kalamatase@hotmail.gr>
2023-06-30 16:15:52 +0300

    Implementing the book creation function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
 # Route to retrieve a list of books
-@app.route('/books')
+@app.route('/books', methods=['GET'])
 def retrieve_books():
     try:
         # Connect to the database
@@ -35,6 +35,41 @@ def retrieve_books():
     except sqlite3.Error as error:
         print("Error while connecting to sqlite database", error)
 
+# Route to create a new book    
+@app.route('/books', methods=['POST'])
+def create_book():
+    # Get the required information from the submission form
+    title = request.form.get('title')
+    author = request.form.get('author')
+    price = request.form.get('price')
+    description = request.form.get('description')
+    
+    # Checking if all required fields are given
+    if not title or not author or not price:
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    try:
+        #Connect to the 'books' database
+        connection = sqlite3.connect('books.db')
+        cursor = connection.cursor()
+        
+        # Add the new book to the table
+        cursor.execute("INSERT INTO books (title, author, price, description) VALUES (?, ?, ?, ?)",
+                (title, author, price, description))
+
+        # Commit changes and close the database
+        connection.commit()
+        connection.close()
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite database", error)
+    
+    # Return a message that the book was successfully created
+    return jsonify({'message': 'The book was successfully created!'}), 201
+
+# Route to render the html.file for the book creation form
+@app.route('/create_book', methods=['GET'])
+def render_book_creation():
+    return render_template('book_creation.html')
 # Running the app
 if __name__ == "__main__":
     app.run()
